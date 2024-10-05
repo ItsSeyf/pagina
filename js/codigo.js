25c26a75b49d79fd023595bace2f1ad769e00b2a
ItsSeyf <michael.mendezb27@gmail.com>
2024-10-04 23:21:18 -0600

    Add files via upload

--- js/codigo.js
+++
@@ -1,7 +0,0 @@
-function whats(r){
-    if (r==1){
-            window.location.href="https://wa.me/50687753408/?text=%C2%A1Hola!"
-    }else{
-        window.location.href="https://wa.me/50671378413/?text=%C2%A1Hola!"
-    }
-}
