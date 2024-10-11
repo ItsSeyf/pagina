831cddacb8a97a71ac2b1f162adfd77bf66217ca
ItsSeyf <michael.mendezb27@gmail.com>
2024-10-11 01:39:18 -0600

    Add files via upload

--- js/codigo.js
+++ js/codigo.js
@@ -7,3 +7,48 @@ function cerrarbarrala(){
     const barrala=document.querySelector('.barrala')
     barrala.style.display='none'
 }
+
+async function enviar(event) {
+    event.preventDefault();
+
+    const datos = {
+        nombre: document.getElementById('Nombre').value,
+        opcion: document.getElementById('opciones').value,
+        nece: document.getElementById('Necesidad').value,
+        contacto: document.getElementById('contacto').value,
+        infocon: document.getElementById('infocon').value
+    };
+
+    try {
+        const response = await fetch('https://formspree.io/f/mwpkeggl', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(datos),
+        });
+
+
+        if (response.ok) {
+            const resultado = await response.json();
+            alert('Formulario enviado con éxito');
+        } else {
+            const errorMsg = await response.text();
+            console.error('Error en la respuesta de la API:', errorMsg);
+            alert('Ocurrió un problema: ' + errorMsg);
+        }
+    } catch (error) {
+        console.error('Error capturado:', error);
+        alert('Ocurrió un error, intente nuevamente');
+    }
+}
+
+function vaciar(event){
+    event.preventDefault();
+    document.getElementById('Nombre').value="";
+    document.getElementById('opciones').value=1;
+    document.getElementById('Necesidad').value="";
+    document.getElementById('contacto').value=1;
+    document.getElementById('infocon').value="";
+}
